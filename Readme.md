c67b6da7d15b1113562b0f35971065642841d51d
Thabang Thamane <thabang.biz@gmail.com>
2021-10-23 18:26:52 +0200

    First implementation of stack and queue
    Spent about 4 hours while multi-tasking. This is still a slow process as I am
    making silly errors and still trying to get my eyes used to racket.
    
    The feeling is not so bad at the moment.

--- Readme.md
+++ Readme.md
@@ -1,6 +1,14 @@
+### Note:
+I am not a racket developer and using this to learn the language. I am beginner to racket/list so those files
+will be in beginner. Maybe I'll create news versions once I'm intermediate (unless I don't need to test my improvedment).
+I do not intend to be a racket developer as I am using this language to learn something different to what I am used. In the
+past I loved PHP and never used it once I got used to Ruby and then Python. I am not saying that will be the case but
+I am looking for something that looks different and will force me to live in a "recursive" world.
+
+### The rest
 Another attempt at writing data structures and algorithms in a functional language. I lost momentum quickly with 
 OCAML hopefully using Racket will be a different story. Once I get used to racket, I might have another look
-at ocaml once I can think in a functional way.
+at ocaml if I can think in a functional way.
 
 For now I am trying to avoid languages with loops prefering recursion first. I will got through the data structures
 and algorithms in python first while writing the implementation in racket instead of python.
---
+++ beginner/stack-queue-lists.rkt
@@ -0,0 +1,109 @@
+#lang racket
+
+; Node struct
+(define-struct node (value [next #:mutable]))
+
+
+; Create a basic stack implementation
+#|
+	Avoiding using the simple primitives provided by the library to
+	mimick how I would build it else
+|#
+; Stack struct
+(define-struct stack ([root #:mutable]))
+
+; Push value on to stack
+(define (stack-push! S val)
+  (define newRoot (make-node val (stack-root S)))
+  (set-stack-root! S newRoot))
+
+; Pop value from stack
+(define (stack-pop! S)
+  (if (null? (stack-root S))
+      '()
+      (let (
+            [ptr (stack-root S)])
+        (set-stack-root! S (node-next ptr))
+        (node-value ptr))))
+
+; Print the stack values as a list
+(define (stack->list S)
+  (define (_stack->list node acc)
+    (if (null? node)
+        acc
+        (_stack->list (node-next node) (cons (node-value node) acc))))
+  (let (
+        [ptr (stack-root S)]
+        [acc '()])
+    (reverse (_stack->list ptr acc))))
+
+(displayln "===== Testing stack =====\n")
+(define s1 (make-stack '()))
+(displayln (stack->list s1))
+(stack-push! s1 10)
+(displayln (stack->list s1))
+(stack-push! s1 20)
+(displayln (stack->list s1))
+(stack-push! s1 30)
+(displayln (stack->list s1))
+(stack-pop! s1)
+(displayln (stack->list s1))
+(stack-pop! s1)
+(displayln (stack->list s1))
+(stack-pop! s1)
+(displayln (stack->list s1))
+
+(displayln "\n===== End of stack test =====")
+
+
+;; Create a basic queue implementation
+
+; Queue struct
+(define-struct queue ([root #:mutable]))
+
+; Add value to enqueue
+(define (queue->enqueue! Q val)
+  (define newNode (make-node val '()))
+  (define (add-to-leaf ptr newNode)
+    (if (null? (node-next ptr))
+        (set-node-next! ptr newNode)
+        (add-to-leaf (node-next ptr) newNode)))
+  (if (null? (queue-root Q))
+      (set-queue-root! Q newNode)
+      (add-to-leaf (queue-root Q) newNode)))
+  
+; Remove value from queue
+; Notice: Stack and Queue remove an item in the exact same way (Just noticed this)
+(define (queue->dequeue! Q)
+  (define ptr (queue-root Q))
+  (set-queue-root! Q (node-next ptr))
+  (node-value ptr))
+
+; Create array from queue
+(define (queue->list Q)
+  (define (_queue->list node acc)
+    (if (null? node)
+        acc
+        (_queue->list (node-next node) (cons (node-value node) acc))))
+  (let ([acc '()])
+    (if (null? (queue-root Q))
+        '()
+        (reverse (_queue->list (queue-root Q) acc)))))
+
+(displayln "===== Testing stack =====\n")
+(define q1 (make-queue'()))
+(displayln (queue->list q1))
+(queue->enqueue! q1 10)
+(displayln (queue->list q1))
+(queue->enqueue! q1 20)
+(displayln (queue->list q1))
+(queue->enqueue! q1 30)
+(displayln (queue->list q1))
+(queue->dequeue! q1)
+(displayln (queue->list q1))
+(queue->dequeue! q1)
+(displayln (queue->list q1))
+(queue->dequeue! q1)
+(displayln (queue->list q1))
+
+;; Create a basic linked-list implementation
