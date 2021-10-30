567150b36a59ee7d2513884e4917a48960df813e
Thabang Thamane <thabang.biz@gmail.com>
2021-10-31 00:13:16 +0200

    Add support for adding an item to the heap

--- beginner/f3-heaps.rkt
+++ beginner/f3-heaps.rkt
@@ -2,41 +2,70 @@
 
 (require "utils.rkt")
 
-(define-struct max-heap ([size #:mutable] [capacity #:mutable] [values #:mutable]))
+;; This code works but confuses me too.
 
-(define (change-value-at-index L index value)
-	(define (_change-value-at-index oldList currentIndex targetList)
-    (cond 
-    [(null? oldList) targetList]
-    [(= currentIndex index)
-      (_change-value-at-index (rest oldList) (+ currentIndex 1) (cons value targetList))]
-      [else (change-value-at-index (rest oldList) (+ currentIndex 1)) (cons (first oldList) targetList)]))
-  (t-reverse (_change-value-at-index L index '())))
+(define-struct max-heap ([values #:mutable]))
+
+; Swap two indexes in an array
+(define (swap-at-indexes L firstIndex secondIndex)
+  (define firstValue (list-ref L firstIndex))
+  (define secondValue (list-ref L secondIndex))
+  (define (_change-at-index l index value)
+    (define firstPart (take l index))
+    (define lastPart (drop l (+ 1 index)))
+    (append firstPart (list value) lastPart))
+  (if (= (t-length L) 2)
+      (list secondValue firstValue)
+      (let* ([firstValueSwapped (_change-at-index L firstIndex secondValue)]
+             [secondValueSwapped (_change-at-index firstValueSwapped secondIndex firstValue)])
+        secondValueSwapped)))
 
 
-(define (max-heap-ensure-capacity H)
-  (define size (max-heap-size H))
-  (define capacity (max-heap-capacity H))
+
+(define (max-heap-add H val)
   (define values (max-heap-values H))
-  (when (= capacity size)
-      (set-max-heap-values! H (t-grow-list values (* 2 capacity)))
-      ))
+  (define newValues (append values (list val)))
+  (set-max-heap-values! H newValues)
+  (define lastIndex (- (t-length newValues) 1))
+  (when (> lastIndex 0)
+    (max-heap-shuffle-up H lastIndex)))
+
+(define (get-parent-index index)
+  (if (odd? index)
+      (- (/ (+ index 1) 2) 1)
+      (- (/ index 2) 1)))
 
 
-; (define (max-heap-add H val)
-;   (define heapSize (max-heap-size H))
-;   (define heapCapacity (max-heap-capacity H))
-;   (when (= heapSize heapCapacity)
+(define (max-heap-shuffle-up H startIndex)
+  (define parentIndex (get-parent-index startIndex))
+  (define values (max-heap-values H))
+  (define parentValue (list-ref values parentIndex))
+  (define indexValue (list-ref values startIndex))
+  (when (> indexValue parentValue)
+    (begin
+      (set-max-heap-values! H (swap-at-indexes values parentIndex startIndex))
+      (when (> parentIndex 0)
+        (max-heap-shuffle-up H parentIndex)))))
+
+;; (define (max-heap-shuffle-down H startIndex)
+;;   '())
 
 
+(displayln "===== Adding to a max-heap =====")
 
-(displayln "===== Testing Current List Capacity =====")
+(define heap1 (make-max-heap '()))
 
-(define heap1 (make-max-heap 0 8 (t-make-list 8)))
 (displayln (max-heap-values heap1))
 
-(max-heap-ensure-capacity heap1)
-(displayln (max-heap-capacity heap1))
-
-
+(max-heap-add heap1 10)
+(displayln (max-heap-values heap1))
+(max-heap-add heap1 20)
+(displayln (max-heap-values heap1))
+(max-heap-add heap1 15)
+(displayln (max-heap-values heap1))
+(max-heap-add heap1 25)
+(displayln (max-heap-values heap1))
+(max-heap-add heap1 22)
+(displayln (max-heap-values heap1))
+(max-heap-add heap1 50)
 (displayln (max-heap-values heap1))
